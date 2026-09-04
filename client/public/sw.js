48f093b0bf4c717a2c0ab7434b378356285e1fe7
diegomes42 <diegomes42@gmail.com>
2026-09-04 08:57:48 +0000

    fix: atualizar cache após correção do ícone NF

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "negocio-fechado-shell-v9";
+const CACHE_NAME = "negocio-fechado-shell-v10";
 const APP_SHELL = ["./", "./index.html"];
 const BYPASS_PREFIXES = ["/api/", "/manus-storage/"];
 
